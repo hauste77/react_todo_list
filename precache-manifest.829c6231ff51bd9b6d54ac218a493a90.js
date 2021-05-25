9cdce862b66e6a435919bfb695e338b55a08f1a6
xavier <xav.77176@hotmail.fr>
2021-05-25 11:25:28 +0200

    Updates

---
+++ precache-manifest.829c6231ff51bd9b6d54ac218a493a90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cfee7637a2aedbda82c267aed1552f4",
+    "url": "/react_todo_list/index.html"
+  },
+  {
+    "revision": "c8a572154f7c14ef4073",
+    "url": "/react_todo_list/static/css/main.3ba1893e.chunk.css"
+  },
+  {
+    "revision": "fda7aeacd84abf9db942",
+    "url": "/react_todo_list/static/js/2.5de09e7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_todo_list/static/js/2.5de09e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a572154f7c14ef4073",
+    "url": "/react_todo_list/static/js/main.cee2ca4d.chunk.js"
+  },
+  {
+    "revision": "1cde16d6c5941c715789",
+    "url": "/react_todo_list/static/js/runtime-main.dfa3a831.js"
+  }
+]);
